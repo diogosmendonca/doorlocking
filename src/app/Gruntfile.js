e2e49d16d9d2a433863575211779ef5b5746a706
Diogo S. Mendonca <diogosmendonca@gmail.com>
2019-08-07 15:39:12 -0300

    Testing Pure instead of Bootstrap to reduce application size

--- src/app/Gruntfile.js
+++ src/app/Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt){
         sass: {
             dist: {
                 files: {
-                    'css/styles.css': 'css/styles.scss'
+                    'css/styles.css': 'css/styles.scss',
+                    'css/side-menu.css': 'css/side-menu.scss',
                 }
             }
         },
